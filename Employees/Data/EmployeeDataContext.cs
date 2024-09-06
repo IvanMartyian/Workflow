c2ed1d26850e5ce7928bc450223bbabd57ccb542
Martyian <Ivan.Martyian@audius.de>
2024-09-06 13:47:04 +0200

    first commit

---
+++ Employees/Data/EmployeeDataContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Employees.Models;
+
+namespace Employees.Data
+{
+    public class EmployeeDataContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public EmployeeDataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(Configuration.GetConnectionString("EmployeeDB"));
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>()
+                .ToTable("Employee");
+
+            modelBuilder.Entity<Employee>()
+                .HasData(
+                // Hier kannst du Seed-Daten hinzufügen, falls gewünscht
+                );
+        }
+    }
+}
