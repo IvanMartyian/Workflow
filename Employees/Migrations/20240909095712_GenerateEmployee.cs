38819e160deff86afc7f2f41d5de693e405bb7a6
Martyian <Ivan.Martyian@audius.de>
2024-09-09 12:01:16 +0200

    Fifth commit

---
+++ Employees/Migrations/20240909095712_GenerateEmployee.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Employees.Migrations
+{
+    /// <inheritdoc />
+    public partial class GenerateEmployee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Avatar",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "Department",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Employee");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Employee",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Employee",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthDate",
+                table: "Employee",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Employee",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "BirthDate",
+                value: new DateTime(1980, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Employee",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "BirthDate",
+                value: new DateTime(1990, 2, 2, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BirthDate",
+                table: "Employee");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Employee",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Employee",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Avatar",
+                table: "Employee",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Department",
+                table: "Employee",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Employee",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Employee",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Avatar", "Department", "Email" },
+                values: new object[] { "https://example.com/avatars/1.png", "Sales", "max.muster@example.com" });
+
+            migrationBuilder.UpdateData(
+                table: "Employee",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Avatar", "Department", "Email" },
+                values: new object[] { "https://example.com/avatars/2.png", "Marketing", "erika.mustermann@example.com" });
+        }
+    }
+}
